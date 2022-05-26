4c47b6197770ced1c5def408c9ccd9836491d00e
YiLin Ong <chauntelleinc@gmail.com>
2022-05-26 12:32:16 -0400

    added logic to method that accepts content and add to db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,8 +13,14 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => 
-
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readWrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1, value: content });
+  console.log(': rocket: - data saved to the database', result.value);
+}; 
 
 
 // TODO: Add logic for a method that gets all the content from the database
