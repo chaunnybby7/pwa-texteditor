954eb8e750ba8b7f454c2ab7cc21088b59a38324
YiLin Ong <chauntelleinc@gmail.com>
2022-05-26 12:32:41 -0400

    added logic to get all content from db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -24,5 +24,16 @@ export const putDb = async (content) => {
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => 
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log(':rocket: - data retrived from the database', result.value)
+    : console.log(':rocket: - data not found in the database');
+    return result?.value
+}; 
 initdb();
